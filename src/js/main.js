4ad1fe52e8865104d57c3941a698269b78057d42
Mürsel Şen <46069077+murselsen@users.noreply.github.com>
2025-01-08 12:12:56 +0300

    Update main.js to replace organizations and features sections with specials, service, and menu sections

--- src/js/main.js
+++ src/js/main.js
@@ -3,12 +3,12 @@ $(document).ready(() => {
   $('#page-header').load('src/partials/header.html');
   // Hero
   $('#hero').load('src/partials/hero.html');
-  // Organizations
-  $('#organizations').load('src/partials/organizations.html');
-  // Features
-  $('#features').load('src/partials/features.html');
-  // Downloads
-  $('#downloads').load('src/partials/downloads.html');
+  // Specials
+  $('#special').load('src/partials/special.html');
+  // Service
+  $('#service').load('src/partials/service.html');
+  // Menu
+  $('#menu').load('src/partials/menu.html');
   // Footer
   $('#page-footer').load('src/partials/footer.html');
 });
